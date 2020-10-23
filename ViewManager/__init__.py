76c1326052630cb69876bb68f0fa4c2932b8804b
Jim Miller <retiefjimm@gmail.com>
2020-10-23 10:20:21 -0500

    VM: Bump test version.

--- ViewManager/__init__.py
+++ ViewManager/__init__.py
@@ -24,7 +24,7 @@ class ActionViewManager(InterfaceActionBase):
     description             = 'Define your own sets of column views with sort orders'
     supported_platforms     = ['windows', 'osx', 'linux']
     author                  = 'Grant Drake'
-    version                 = (1, 5, 7)
+    version                 = (1, 5, 8)
     minimum_calibre_version = (2, 85, 1)
 
     #: This field defines the GUI plugin class that contains all the code
