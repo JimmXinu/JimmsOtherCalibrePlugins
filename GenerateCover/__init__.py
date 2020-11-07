161d23ac16ee7413331dad4420f9b13b7079f4a8
Jim Miller <retiefjimm@gmail.com>
2020-11-07 12:52:46 -0600

    GC: Bump release version v2.0.0 in honor of retiring draw_old.py

--- GenerateCover/__init__.py
+++ GenerateCover/__init__.py
@@ -29,7 +29,7 @@ class ActionGenerateCover(InterfaceActionBase):
     description             = _('Generate covers in Calibre with appearance options')
     supported_platforms     = ['windows', 'osx', 'linux']
     author                  = 'Grant Drake'
-    version                 = (1, 9, 3)
+    version                 = (2, 0, 0)
     minimum_calibre_version = (2, 85, 1)
 
     #: This field defines the GUI plugin class that contains all the code
