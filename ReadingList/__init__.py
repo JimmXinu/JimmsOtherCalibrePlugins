52d27060a91e3642f043f567537ca131540ffcd6
Jim Miller <retiefjimm@gmail.com>
2022-01-12 11:05:34 -0600

    RL: Bump test version.

--- ReadingList/__init__.py
+++ ReadingList/__init__.py
@@ -24,7 +24,7 @@ class ActionReadingList(InterfaceActionBase):
     description             = 'Define orderable lists of books and synchronise to devices/folders'
     supported_platforms     = ['windows', 'osx', 'linux']
     author                  = 'Grant Drake'
-    version                 = (1, 7, 7)
+    version                 = (1, 7, 8)
     minimum_calibre_version = (2, 85, 1)
 
     #: This field defines the GUI plugin class that contains all the code
