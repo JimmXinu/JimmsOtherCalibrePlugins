a73de60454659de353cbce48c136465848e6e663
Jim Miller <retiefjimm@gmail.com>
2022-07-20 13:32:11 -0500

    FM/GC/MS/RL/VM - Use Cal6 get_icons() so icon themes apply.

--- GenerateCover/common_utils.py
+++ GenerateCover/common_utils.py
@@ -21,6 +21,7 @@ from PyQt5.Qt import (Qt, QIcon, QPixmap, QLabel, QDialog, QHBoxLayout,
                       QVBoxLayout, QDialogButtonBox, QStyledItemDelegate, QDateTime,
                       QTextEdit, QListWidget, QAbstractItemView)
 
+from calibre.constants import numeric_version as calibre_version
 from calibre.constants import iswindows
 from calibre.gui2 import gprefs, error_dialog, UNDEFINED_QDATETIME, info_dialog
 from calibre.gui2.actions import menu_action_unique_name
@@ -48,8 +49,38 @@ def set_plugin_icon_resources(name, resources):
     plugin_name = name
     plugin_icon_resources = resources
 
+# print_tracebacks_for_missing_resources first appears in cal 6.2.0
+if calibre_version >= (6,2,0):
+    def get_icons_nolog(icon_name,plugin_name):
+        return get_icons(icon_name,
+                         plugin_name,
+                         print_tracebacks_for_missing_resources=False)
+else:
+    get_icons_nolog = get_icons
 
-def get_icon(icon_name):
+def get_icon_6plus(icon_name):
+    '''
+    Retrieve a QIcon for the named image from
+    1. Calibre's image cache
+    2. resources/images
+    3. the icon theme
+    4. the plugin zip
+    Only plugin zip has images/ in the image name for backward
+    compatibility.
+    '''
+    if icon_name:
+        icon = QIcon.ic(icon_name)
+        ## both .ic and get_icons return an empty QIcon if not found.
+        if not icon or icon.isNull():
+            # don't need a tracestack from get_icons just because
+            # there's no icon in the theme
+            icon = get_icons_nolog(icon_name.replace('images/',''),
+                                   plugin_name)
+        if not icon or icon.isNull():
+            icon = get_icons(icon_name,plugin_name)
+    return icon
+
+def get_icon_old(icon_name):
     '''
     Retrieve a QIcon for the named image from the zip file if it exists,
     or if not then from Calibre's image cache.
@@ -63,6 +94,11 @@ def get_icon(icon_name):
             return QIcon(pixmap)
     return QIcon()
 
+# get_icons changed in Cal6.
+if calibre_version >= (6,0,0):
+    get_icon = get_icon_6plus
+else:
+    get_icon = get_icon_old
 
 def get_pixmap(icon_name):
     '''
