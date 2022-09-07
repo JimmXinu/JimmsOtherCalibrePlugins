8b2fbb86fd72381162240e34bb171422990c051a
Jim Miller <retiefjimm@gmail.com>
2022-09-07 17:15:23 -0500

    GC: Bump test version

--- GenerateCover/__init__.py
+++ GenerateCover/__init__.py
@@ -29,7 +29,7 @@ class ActionGenerateCover(InterfaceActionBase):
     description             = _('Generate covers in Calibre with appearance options')
     supported_platforms     = ['windows', 'osx', 'linux']
     author                  = 'Grant Drake'
-    version                 = (2, 2, 1)
+    version                 = (2, 2, 2)
     minimum_calibre_version = (2, 85, 1)
 
     #: This field defines the GUI plugin class that contains all the code
