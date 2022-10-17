229114373c9eed2b1175b89e848d018d7c3f787c
Jim Miller <retiefjimm@gmail.com>
2022-10-17 16:55:30 -0500

    Remove kiwidudes plugins, he's active again.

--- GenerateCover/makemessages.bat
+++
@@ -1,3 +0,0 @@
-python C:\Python38\Tools\i18n\pygettext.py __init__.py action.py common_utils.py config.py dialogs.py
-mv messages.pot translations\default.pot
-
--- GenerateCover/maketranslations.bat
+++
@@ -1,6 +0,0 @@
-cd translations
-for %%f in (*.po) do (
-    "C:\Program Files\Calibre2\calibre-debug.exe" -c "from calibre.translations.msgfmt import main; main()" %%~nf
-)
-
-cd ..
