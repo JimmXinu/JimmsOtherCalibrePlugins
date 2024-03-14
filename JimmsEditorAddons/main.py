d8e2361ae9f8403a5dc95d705fc2b63b23ecc819
Jim Miller <retiefjimm@gmail.com>
2024-03-13 22:04:21 -0500

    Add Editor feature to add file, title and add TOC entry for same in one step.

--- JimmsEditorAddons/main.py
+++ JimmsEditorAddons/main.py
@@ -6,18 +6,33 @@ from __future__ import (unicode_literals, division, absolute_import,
 __license__   = 'GPL v3'
 __docformat__ = 'restructuredtext en'
 
-from PyQt5.Qt import QAction, QMenu, QDialog, QIcon, QPixmap
+from qt.core import ( QAction, QMenu, QDialog, QIcon, QPixmap,
+                      QVBoxLayout, QLabel, QLineEdit, QCheckBox,
+                      QDialogButtonBox
+                       )
 
 import os
 
 # The base class that all tools must inherit from
 from calibre.gui2.tweak_book.plugin import Tool
 
-from calibre.gui2.tweak_book import current_container
+from calibre.gui2.tweak_book import current_container, tprefs
+from calibre.gui2.tweak_book.templates import template_for
+#from calibre.gui2.tweak_book.file_list import FILE_COPY_MIME, NewFileDialog
+
+from calibre.ebooks.oeb.base import OEB_DOCS, OEB_STYLES
+from calibre.ebooks.oeb.polish.css import add_stylesheet_links
 from calibre.ebooks.oeb.polish.container import get_container as _gc
+from calibre.ebooks.oeb.polish.utils import OEB_FONTS, guess_type
+from calibre.ebooks.oeb.polish.toc import (
+    TOC, get_toc, commit_toc
+)
+
 from calibre.ptempfile import TemporaryDirectory
-from calibre.gui2.tweak_book.file_list import NAME_ROLE
+from calibre.gui2.tweak_book.file_list import NAME_ROLE, name_is_ok
 from calibre.gui2 import error_dialog
+from calibre.gui2.widgets import BusyCursor
+
 from polyglot.builtins import unicode_type
 
 def get_icon(icon_name):
@@ -47,7 +62,7 @@ class DeleteFile(Tool):
 
     def delete_file(self):
         print(self.boss.gui.file_list.file_list.current_edited_name)
-        
+
         self.request_delete(self.boss.gui.file_list.file_list)
 
     # inspired by calibre.gui2.tweak_book.file_list.request_delete()
@@ -61,7 +76,7 @@ class DeleteFile(Tool):
 
         spine_removals = []
         other_removals = set()
-        
+
         found = False
         treetext = file_list.categories['text']
         for i in range(treetext.childCount()):
@@ -73,7 +88,7 @@ class DeleteFile(Tool):
         if not found:
             ## if it's not a spine object, assume 'other'
             other_removals.add(name)
-            
+
         print(spine_removals)
         print(other_removals)
         self.boss.delete_requested(spine_removals, other_removals)
@@ -142,3 +157,152 @@ class CompareOriginalFile(Tool):
             d = self.boss.create_diff_dialog(revert_msg=None)
             d.container_diff(other, c,
                              names=(_('Original File'), _('Current book')))
+
+class AddNewFileTOC(Tool):
+    name = 'Add New File and TOC'
+
+    #: If True the user can choose to place this tool in the plugins toolbar
+    allowed_in_toolbar = True
+
+    #: If True the user can choose to place this tool in the plugins menu
+    allowed_in_menu = True
+
+    def create_action(self, for_toolbar=True):
+        # Create an action, this will be added to the plugins toolbar and
+        # the plugins menu
+        ac = QAction(get_icon('document-new.png'), _('Add New File to book and TOC'), self.gui)
+        if not for_toolbar:
+            # Register a keyboard shortcut for this toolbar action. We only
+            # register it for the action created for the menu, not the toolbar,
+            # to avoid a double trigger
+            self.register_shortcut(ac, 'add-file-and-toc-file', default_keys=('Ctrl+Num++',))
+        ac.triggered.connect(self.add_file_and_toc)
+        return ac
+
+    def add_file_and_toc(self):
+        if not self.boss.ensure_book(_('You must first open a book to edit, before trying to create new files in it.')):
+            return
+        self.boss.commit_dirty_opf()
+        d = NewFileDialog(self.boss.gui)
+        if d.exec() != QDialog.DialogCode.Accepted:
+            return
+        added_name = self.boss.do_add_file(d.file_name, d.file_data, using_template=d.using_template, edit_file=True)
+        if d.file_name.rpartition('.')[2].lower() in ('ttf', 'otf', 'woff'):
+            from calibre.gui2.tweak_book.manage_fonts import (
+                show_font_face_rule_for_font_file,
+            )
+            show_font_face_rule_for_font_file(d.file_data, added_name, self.boss.gui)
+        if d.title.text():
+            # frag = add_id(self.ebook, name, *frag)
+            # container, name, loc
+            with BusyCursor():
+                toc = get_toc(current_container())
+                print(toc)
+                toc.add(d.title.text(), d.file_name)
+                print(toc)
+                commit_toc(current_container(),
+                           toc,
+                           lang=toc.lang,
+                           uid=toc.uid)
+                current_container().commit()
+                self.boss.set_modified()
+                self.boss.update_editors_from_container()
+                self.boss.gui.toc_view.update_if_visible()
+                self.boss.gui.file_list.build(current_container())
+
+class NewFileDialog(QDialog):  # {{{
+
+    def __init__(self, parent=None):
+        QDialog.__init__(self, parent)
+        self.l = l = QVBoxLayout()
+        self.setLayout(l)
+        self.la = la = QLabel(_(
+            'Choose a name for the new (blank) file. To place the file in a'
+            ' specific folder in the book, include the folder name, for example: <i>text/chapter1.html'))
+        la.setWordWrap(True)
+        self.setWindowTitle(_('Choose file'))
+        l.addWidget(la)
+        self.name = n = QLineEdit(self)
+        n.textChanged.connect(self.update_ok)
+        l.addWidget(n)
+        self.la = la = QLabel(_(
+            'Choose a chapter title (replaces %CHAPTER% in template) and for ToC for example: <i>Chapter 1</i>'))
+        la.setWordWrap(True)
+        l.addWidget(la)
+        self.title = t = QLineEdit(self)
+        l.addWidget(t)
+        self.link_css = lc = QCheckBox(_('Automatically add style-sheet links into new HTML files'))
+        lc.setChecked(tprefs['auto_link_stylesheets'])
+        l.addWidget(lc)
+        self.err_label = la = QLabel('')
+        la.setWordWrap(True)
+        l.addWidget(la)
+        self.bb = bb = QDialogButtonBox(QDialogButtonBox.StandardButton.Ok | QDialogButtonBox.StandardButton.Cancel)
+        l.addWidget(bb)
+        bb.accepted.connect(self.accept)
+        bb.rejected.connect(self.reject)
+        self.imp_button = b = bb.addButton(_('Import resource file (image/font/etc.)'), QDialogButtonBox.ButtonRole.ActionRole)
+        b.setIcon(QIcon.ic('view-image.png'))
+        b.setToolTip(_('Import a file from your computer as a new'
+                       ' file into the book.'))
+        b.clicked.connect(self.import_file)
+
+        self.ok_button = bb.button(QDialogButtonBox.StandardButton.Ok)
+
+        self.file_data = b''
+        self.using_template = False
+        self.setMinimumWidth(350)
+
+    def show_error(self, msg):
+        self.err_label.setText('<p style="color:red">' + msg)
+        return False
+
+    def import_file(self):
+        path = choose_files(self, 'tweak-book-new-resource-file', _('Choose file'), select_only_single_file=True)
+        if path:
+            self.do_import_file(path[0])
+
+    def do_import_file(self, path, hide_button=False):
+        self.link_css.setVisible(False)
+        with open(path, 'rb') as f:
+            self.file_data = f.read()
+        name = os.path.basename(path)
+        fmap = get_recommended_folders(current_container(), (name,))
+        if fmap[name]:
+            name = '/'.join((fmap[name], name))
+        self.name.setText(name)
+        self.la.setText(_('Choose a name for the imported file'))
+        if hide_button:
+            self.imp_button.setVisible(False)
+
+    @property
+    def name_is_ok(self):
+        return name_is_ok(str(self.name.text()), self.show_error)
+
+    def update_ok(self, *args):
+        self.ok_button.setEnabled(self.name_is_ok)
+
+    def accept(self):
+        if not self.name_is_ok:
+            return error_dialog(self, _('No name specified'), _(
+                'You must specify a name for the new file, with an extension, for example, chapter1.html'), show=True)
+        tprefs['auto_link_stylesheets'] = self.link_css.isChecked()
+        print("title:%s"%self.title.text())
+        name = str(self.name.text())
+        name, ext = name.rpartition('.')[0::2]
+        name = (name + '.' + ext.lower()).replace('\\', '/')
+        mt = guess_type(name)
+        if not self.file_data:
+            if mt in OEB_DOCS:
+                self.file_data = template_for('html').replace('%CHAPTER%',self.title.text()).encode('utf-8')
+                if tprefs['auto_link_stylesheets']:
+                    data = add_stylesheet_links(current_container(), name, self.file_data)
+                    if data is not None:
+                        self.file_data = data
+                self.using_template = True
+            elif mt in OEB_STYLES:
+                self.file_data = template_for('css').encode('utf-8')
+                self.using_template = True
+        self.file_name = name
+        QDialog.accept(self)
+# }}}
