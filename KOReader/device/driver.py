b6d62eb864f6e049c6bc6f4709cf502496889945
Jim Miller <retiefjimm@gmail.com>
2025-06-22 12:17:40 -0500

    KR: Switch off cache/history update when config'ed path empty.

--- KOReader/device/driver.py
+++ KOReader/device/driver.py
@@ -28,13 +28,13 @@ class KOREADER(USER_DEFINED):
     name = "KOReader Device Interface"
     gui_name = "KOReader eReader"
     author = "Jim Miller"
-    description = _( 
+    description = _(
         "Communicate with KOReader running (on Kobo?) to make it behave how I want."
     )
     supported_platforms = ['windows', 'osx', 'linux']
 
     minimum_calibre_version = (8,4,0)
-    version = (0,2,0)
+    version = (0,3,0)
 
     ## also delete .sdr 'sidecar' dirs on file delete.
     DELETE_EXTS  = ['.sdr']
@@ -103,7 +103,7 @@ class KOREADER(USER_DEFINED):
 
     def upload_books(self, files, names, on_card=None, end_session=True,
                      metadata=None):
-        logger.debug(f'uploading {len(files)} books')
+        # logger.debug(f'uploading {len(files)} books')
         # logger.debug(f'uploading {files} books')
         # logger.debug(f'uploading {names}')
         # logger.debug(f'uploading {metadata}')
@@ -113,8 +113,14 @@ class KOREADER(USER_DEFINED):
         # logger.debug("KOReader:upload_books: cache:%s"%e[self.OPT_KOREADER_CACHE])
         # logger.debug("KOReader:upload_books: history:%s"%e[self.OPT_KOREADER_HISTORY])
 
-        cache_sql_path = os.path.join(self._main_prefix, e[self.OPT_KOREADER_CACHE])
-        history_lua_path = os.path.join(self._main_prefix, e[self.OPT_KOREADER_HISTORY])
+        cache_sql_path = e[self.OPT_KOREADER_CACHE]
+        if cache_sql_path:
+            cache_sql_path = os.path.join(self._main_prefix,cache_sql_path)
+
+        history_lua_path = e[self.OPT_KOREADER_HISTORY]
+        if history_lua_path:
+            history_lua_path = os.path.join(self._main_prefix, history_lua_path)
+
         onboard_path = e[self.OPT_KOREADER_ONBOARD]
         bump_tag = e[self.OPT_KOREADER_BUMP_TAG]
 
@@ -123,67 +129,68 @@ class KOREADER(USER_DEFINED):
         # logger.debug("KOReader:upload_books: history_lua_path:%s"%history_lua_path)
         # logger.debug("KOReader:upload_books: history_lua_path isfile:%s"%os.path.isfile(history_lua_path))
 
-        db = apsw.Connection(cache_sql_path)
+        db = None
+        if cache_sql_path:
+            db = apsw.Connection(cache_sql_path)
+            ## matching
+            ## 'D:\\calibre\\11956.epub'
+            ## to
+            ## '/mnt/onboard/calibre/', '7158.epub'
+            ##
+            updated_filepaths = []
+            for book in retlist:
+                path, filename = os.path.split(os.path.splitdrive(book[0])[1])
+                path = onboard_path + path.replace('\\','/')
+                if not path.endswith('/'):
+                    path = path + '/'
+                logger.debug("Remove '%s','%s' from cache db"%(path,filename))
 
-        ## matching
-        ## 'D:\\calibre\\11956.epub'
-        ## to
-        ## '/mnt/onboard/calibre/', '7158.epub'
-        ##
-        updated_filepaths = []
-        for book in retlist:
-            path, filename = os.path.split(os.path.splitdrive(book[0])[1])
-            path = onboard_path + path.replace('\\','/')
-            if not path.endswith('/'):
-                path = path + '/'
-            logger.debug("'%s','%s'"%(path,filename))
+                # for row in db.execute('select directory, filename, title from bookinfo where directory=? and filename=?',
+                #                       (path, filename)):
+                #     logger.debug(row)
+                db.execute('delete from bookinfo where directory=? and filename=?',
+                           (path, filename))
+                updated_filepaths.append(path+filename)
+            db.close()
 
-            # for row in db.execute('select directory, filename, title from bookinfo where directory=? and filename=?',
-            #                       (path, filename)):
-            #     logger.debug(row)
-            db.execute('delete from bookinfo where directory=? and filename=?',
-                       (path, filename))
-            updated_filepaths.append(path+filename)
-        db.close()
+        if history_lua_path:
+            data = luadata.read(history_lua_path, encoding="utf-8")
+            ## [
+            ##   {'file': '/mnt/onboard/.adds/koreader/help/quickstart-en-v2025.04.html',
+            ##    'time': 1750010070
+            ##   },
+            ##   {'file': ## '/mnt/onboard/calibre/8876.epub',
+            ##    'time': 1749698045
+            ##   },
+            ##   {'file': '/mnt/onboard/calibre/12805.epub',
+            ##    'time': 1750009108
+            ##   }, ... ]
 
-        data = luadata.read(history_lua_path, encoding="utf-8")
-        ## [
-        ##   {'file': '/mnt/onboard/.adds/koreader/help/quickstart-en-v2025.04.html',
-        ##    'time': 1750010070
-        ##   },
-        ##   {'file': ## '/mnt/onboard/calibre/8876.epub',
-        ##    'time': 1749698045
-        ##   },
-        ##   {'file': '/mnt/onboard/calibre/12805.epub', 
-        ##    'time': 1750009108
-        ##   }, ... ]
+            # convert to an orderedict to key by file and preserve... order
+            odata = OrderedDict([ (x['file'], x) for x in data ])
+            # logger.debug(odata)
 
-        # convert to an orderedict to key by file and preserve... order
-        odata = OrderedDict([ (x['file'], x) for x in data ])
-        # logger.debug(odata)
-
-        bump_list = []
-        if bump_tag:
-            bump_list = [ bump_tag in m.tags for m in reversed(metadata) ]
-        # logger.debug(bump_list)
-        ## history only cares about order, so I don't bother updating
-        ## the time, although it would be easy.
-        logger.debug(updated_filepaths)
-        changed = False
-        for i, b in enumerate(reversed(updated_filepaths)):
-            # always if no bump_tag set.
-            if not bump_tag or bump_list[i]:
-                if b not in odata:
-                    odata[b] = {'file':b,'time':int(datetime.now().timestamp()) }
-                logger.debug('history bump:%s'%b)
-                odata.move_to_end(b,last=False) # and the last shall be first...
-                changed = True
-        if changed:
-            luadata.write(history_lua_path, list(odata.values()), encoding="utf-8",
-                          indent="\t", prefix="return ")
+            bump_list = []
+            if bump_tag:
+                bump_list = [ bump_tag in m.tags for m in reversed(metadata) ]
+            # logger.debug(bump_list)
+            ## history only cares about order, so I don't bother updating
+            ## the time, although it would be easy.
+            # logger.debug(updated_filepaths)
+            changed = False
+            for i, b in enumerate(reversed(updated_filepaths)):
+                # always if no bump_tag set.
+                if not bump_tag or bump_list[i]:
+                    if b not in odata:
+                        odata[b] = {'file':b,'time':int(datetime.now().timestamp()) }
+                    logger.debug('history bump:%s'%b)
+                    odata.move_to_end(b,last=False) # and the last shall be first...
+                    changed = True
+            if changed:
+                luadata.write(history_lua_path, list(odata.values()), encoding="utf-8",
+                              indent="\t", prefix="return ")
         return retlist
 
-    ## XXX Also remove from cache on delete?  Deleting in KOReader
+    ## Also remove from cache on delete?  Deleting in KOReader
     ## doesn't bother, and there is a prune function in koreader.
     ## So, no.
-    
